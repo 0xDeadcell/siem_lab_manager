4103fb85ba81129290332668d0635beea007e296
0xdeadcell <matt2wise@gmail.com>
2022-06-11 14:55:42 -0500

    🐱‍💻 Added argument to bulk convert VMs to OVA format

--- README.md
+++ README.md
@@ -30,8 +30,8 @@ pip install -r requirements.txt
 python manage_homelab.py -h
 
 
-usage: manage_homelab.py [-h] [-d VM_DIRECTORY_PATH] [-t]
-                         (--delete_vms | --start_vms | --stop_vms | --suspend_vms | --create_snapshot CREATE_SNAPSHOT | --restore_snapshot RESTORE_SNAPSHOT | --delete_snapshot DELETE_SNAPSHOT | --list_snapshots | -l)
+usage: manage_homelab.py [-h] [-d VM_DIRECTORY_PATH] [-t | -r]
+                         (--delete_vms | --start_vms | --stop_vms | --suspend_vms | --create_snapshot CREATE_SNAPSHOT | --restore_snapshot RESTORE_SNAPSHOT | --delete_snapshot DELETE_SNAPSHOT | --list_snapshots | -l | --convert_to_ova)
 
 Download and control a homelab all from the comfort of your terminal.
 
@@ -39,7 +39,8 @@ options:
   -h, --help            show this help message and exit
   -d VM_DIRECTORY_PATH, --vm_directory_path VM_DIRECTORY_PATH
                         The path where your VMs are/will be stored.
-  -t, --target-running  Set all other commands to target only running VMs
+  -t, --target_running  Set all other commands to target only running VMs
+  -r, --recursive       Set the search for VMXs to be recursive from the specified directory
   --delete_vms          Delete the targeted VMs
   --start_vms           Start the targeted VMs
   --stop_vms            Stop the targeted VMs
@@ -53,6 +54,7 @@ options:
   --list_snapshots      Output a list of snapshots for all targeted VMs
   -l, --list_running_vms
                         List all running VMs
+  --convert_to_ova      Attempt to stop a VM, and convert it to OVA format
 ```
 
 ## Demo
--- manage_homelab.py
+++ manage_homelab.py
@@ -14,6 +14,10 @@ except FileNotFoundError:
     key = reg.CreateKey(reg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\VMware, Inc.\\VMware Workstation')
     reg.SetValueEx(key, 'InstallPath', 0, reg.REG_SZ, '"C:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe"')
     reg.CloseKey(key)
+
+    key = reg.CreateKey(reg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\VMware, Inc.\\VMware Workstation\\OVFTool')
+    reg.SetValueEx(key, 'InstallPath', 0, reg.REG_SZ, 'C:\\"Program Files (x86)\\VMware\\VMware Workstation\\OVFTool\\ovftool.exe"')
+    reg.CloseKey(key)
     from pyvmrun import PyVmrun
 
 
@@ -25,14 +29,42 @@ def download_homelab(path, dest_path):
     gdd.download_file_from_google_drive(file_id="https://drive.google.com/file/d/" + path + "?confirm=1", dest_path=dest_path, unzip=True, showsize=True)
 
 
-def get_valid_vmxs(path):
-    valid_vmx_paths = [ pathlib.Path.joinpath(path, f) for f in os.listdir(path) if os.path.splitext(f)[1] in [".vmx"] ] 
+def convert_vmx_to_ova(path, target_running, recursive=False):
+    import winreg as reg
+    key = reg.OpenKeyEx(reg.HKEY_LOCAL_MACHINE, r'SOFTWARE\\VMware, Inc.\\VMware Workstation\\OVFTool')
+    value = reg.QueryValueEx(key, 'InstallPath')[0]
+    if key:
+        reg.CloseKey(key)
+
+    valid_vmx_paths = [path for path in list_running_vms()]
+    if not target_running:
+        # Gets a list of all valid VMX files starting from the top of the path
+        valid_vmx_paths = get_valid_vmxs(path=path, recursive=recursive)
+
+    # Stops all valid VMXs files in the list
+    for current_path in valid_vmx_paths:
+        print(f"[*] Checking if VM at {current_path} is currently running...")
+        if current_path.stem in str(valid_vmx_paths):
+            print(f"[!] {current_path.stem} is currently running, attempting to stop now...")
+            stop_vms(vmx_paths=path, target_running=target_running, recursive=recursive)
+        print(f"[+] Attempting to convert {current_path.name} to OVA format.")
+        print(f'[*] Running: {value} "{current_path}" "{os.path.splitext(current_path)[0]}.ova"')
+        os.system(fr'{value} "{current_path}" "{os.path.splitext(current_path)[0]}.ova"')
+
+
+def get_valid_vmxs(path, recursive=False):
+    # Recursive
+    valid_vmx_paths = []
+    if recursive:
+        valid_vmx_paths = [os.path.join(root, f) for root, dirs, files in os.walk(path) for f in files if os.path.splitext(f)[1] in [".vmx"] ]
+    else:
+        valid_vmx_paths = [ pathlib.Path.joinpath(path, f) for f in os.listdir(path) if os.path.splitext(f)[1] in [".vmx"] ] 
     return valid_vmx_paths
 
 
-def start_vms(vmx_paths):
+def start_vms(vmx_paths, recursive=False):
     # Gets a list of all valid VMX files starting from the top of the path
-    valid_vmx_paths = get_valid_vmxs(path=vmx_paths)
+    valid_vmx_paths = get_valid_vmxs(path=vmx_paths, recursive=recursive)
 
     # Starts all valid VMXs files in the list
     for current_vmx_path in valid_vmx_paths:
@@ -42,39 +74,40 @@ def start_vms(vmx_paths):
         vm.start()
 
 
-def stop_vms(vmx_paths, target_running):
+def stop_vms(vmx_paths, target_running, recursive=False):
     
     if not target_running:
         # Gets a list of all valid VMX files starting from the top of the path
-        valid_vmx_paths = get_valid_vmxs(path=vmx_paths)
+        valid_vmx_paths = get_valid_vmxs(path=vmx_paths, recursive=recursive)
     else:
         valid_vmx_paths = [path for path in list_running_vms()]
     # Stops all valid VMXs files in the list
     for current_vmx_path in valid_vmx_paths:
         vm = PyVmrun(current_vmx_path)
-        print(f"[+] Stopping VM at {current_vmx_path}")
+        
+        print(f"[+] Attempting to stop VM at {current_vmx_path}")
         # Stop the VM
         vm.stop(mode='hard')
         
 
-def suspend_vms(vmx_paths, target_running):
+def suspend_vms(vmx_paths, target_running, recursive=False):
     if not target_running:
         # Gets a list of all valid VMX files starting from the top of the path
-        valid_vmx_paths = get_valid_vmxs(path=vmx_paths)
+        valid_vmx_paths = get_valid_vmxs(path=vmx_paths, recursive=recursive)
     else:
         valid_vmx_paths = [path for path in list_running_vms()]
     # Suspends all valid VMXs files in the list
     for current_vmx_path in valid_vmx_paths:
         vm = PyVmrun(current_vmx_path)
+        print(f"[+] Attempting to suspend VM at {current_vmx_path}")
         # Suspend the VM
-        print(f"[+] Suspending VM at {current_vmx_path}")
         vm.suspend()
 
 
-def delete_vms(vmx_paths, target_running):
+def delete_vms(vmx_paths, target_running, recursive=False):
     if not target_running:
         # Gets a list of all valid VMX files starting from the top of the path
-        valid_vmx_paths = get_valid_vmxs(path=vmx_paths)
+        valid_vmx_paths = get_valid_vmxs(path=vmx_paths, recursive=recursive)
     else:
         valid_vmx_paths = [path for path in list_running_vms()]
 
@@ -82,15 +115,15 @@ def delete_vms(vmx_paths, target_running):
     for current_vmx_path in valid_vmx_paths:
         vm = PyVmrun(current_vmx_path)
 
-        print(f"[+] Delete VM at {current_vmx_path}")
+        print(f"[+] Attempting to delete VM at {current_vmx_path}")
         # Delete the VM
         vm.deleteVM()
 
 
-def create_snapshot(vmx_paths, target_running, snapshot_name):
+def create_snapshot(vmx_paths, target_running, snapshot_name, recursive=False):
     if not target_running:
         # Gets a list of all valid VMX files starting from the top of the path
-        valid_vmx_paths = get_valid_vmxs(path=vmx_paths)
+        valid_vmx_paths = get_valid_vmxs(path=vmx_paths, recursive=recursive)
     else:
         valid_vmx_paths = [path for path in list_running_vms()]
 
@@ -101,12 +134,13 @@ def create_snapshot(vmx_paths, target_running, snapshot_name):
         print(f"[+] Attempting to create snapshot {snapshot_name} for VM at {current_vmx_path}")
         # Create the snapshot
         vm.snapshot(name=snapshot_name)
+        
 
 
-def delete_snapshot(vmx_paths, target_running, snapshot_name):
+def delete_snapshot(vmx_paths, target_running, snapshot_name, recursive=False):
     if not target_running:
         # Gets a list of all valid VMX files starting from the top of the path
-        valid_vmx_paths = get_valid_vmxs(path=vmx_paths)
+        valid_vmx_paths = get_valid_vmxs(path=vmx_paths, recursive=recursive)
     else:
         valid_vmx_paths = [path for path in list_running_vms()]
 
@@ -114,15 +148,15 @@ def delete_snapshot(vmx_paths, target_running, snapshot_name):
     for current_vmx_path in valid_vmx_paths:
         vm = PyVmrun(current_vmx_path)
 
-        print(f"[+] Delete snapshot {snapshot_name} for VM at {current_vmx_path}")
+        print(f"[+] Attempting to delete snapshot {snapshot_name} for VM at {current_vmx_path}")
         # Delete the snapshot
         vm.deleteSnapshot(name=snapshot_name)
 
 
-def restore_snapshot(vmx_paths, target_running, snapshot_name):
+def restore_snapshot(vmx_paths, target_running, snapshot_name, recursive=False):
     if not target_running:
         # Gets a list of all valid VMX files starting from the top of the path
-        valid_vmx_paths = get_valid_vmxs(path=vmx_paths)
+        valid_vmx_paths = get_valid_vmxs(path=vmx_paths, recursive=recursive)
     else:
         valid_vmx_paths = [path for path in list_running_vms()]
 
@@ -135,10 +169,10 @@ def restore_snapshot(vmx_paths, target_running, snapshot_name):
         vm.revertToSnapshot(name=snapshot_name)
 
 
-def list_snapshots(vmx_paths, target_running):
+def list_snapshots(vmx_paths, target_running, recursive=False):
     if not target_running:
         # Gets a list of all valid VMX files starting from the top of the path
-        valid_vmx_paths = get_valid_vmxs(path=vmx_paths)
+        valid_vmx_paths = get_valid_vmxs(path=vmx_paths, recursive=recursive)
     else:
         valid_vmx_paths = [path for path in list_running_vms()]
 
@@ -159,16 +193,21 @@ def list_snapshots(vmx_paths, target_running):
 
 def list_running_vms():
     vm = PyVmrun(vmx="")
-    return list(vm.list().keys())
+    running_vms = list(vm.list().keys())
+    if len(running_vms) == 0:
+        print("[*] No running VMs found")
+    return running_vms
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Download and control a homelab all from the comfort of your terminal.')
+    exclusive_group = parser.add_mutually_exclusive_group(required=False)
     group = parser.add_mutually_exclusive_group(required=True)
-        #group.add_argument("-d", "--vm_directory_path", help="The path where your VMs are/will be stored.", nargs=1, type=pathlib.Path)
-    #group.add_argument("-t", "--target-running", help="Set all other commands to target only running VMs", action="store_true")
+   
     parser.add_argument("-d", "--vm_directory_path", help="The path where your VMs are/will be stored.", default=os.getcwd(), type=pathlib.Path)
-    parser.add_argument("-t", "--target-running", help="Set all other commands to target only running VMs", action="store_true")
+    
+    exclusive_group.add_argument("-t", "--target_running", help="Set all other commands to target only running VMs", action="store_true")
+    exclusive_group.add_argument("-r", "--recursive", help="Set the search for VMXs to be recursive from the specified directory", action="store_true")
 
     #group.add_argument("--download_homelab", help="Google Drive sharing ID to homelab (e.g., 1OgjI4Bw9K4py4eLz2IKz0RzOPzX-RvRH)", action="store")
     group.add_argument("--delete_vms", help="Delete the targeted VMs", action="store_true")
@@ -180,21 +219,29 @@ if __name__ == "__main__":
     group.add_argument("--delete_snapshot", help="Attempt to delete a snapshot for all targeted VMs, requires a snapshot name", type=str)
     group.add_argument("--list_snapshots", help="Output a list of snapshots for all targeted VMs", action="store_true")
     group.add_argument("-l", "--list_running_vms", help="List all running VMs", action="store_true")
+    group.add_argument("--convert_to_ova", help="Attempt to stop a VM, and convert it to OVA format", action="store_true")
     args = parser.parse_args()
 
+    if args.target_running:
+        print("[*] Tool search mode set to target only running VMs")
+    else:
+        print(f"[*] Tool search directory set to {args.vm_directory_path}")
+
+    if args.recursive:
+        print("[*] Tool search mode set to recursive")
 
     if args.start_vms:
-        start_vms(vmx_paths=args.vm_directory_path)
+        start_vms(vmx_paths=args.vm_directory_path, recursive=args.recursive)
 
     if args.delete_vms:
-        stop_vms(vmx_paths=args.vm_directory_path, target_running=args.target_running)
-        delete_vms(vmx_paths=args.vm_directory_path, target_running=args.target_running)
+        stop_vms(vmx_paths=args.vm_directory_path, target_running=args.target_running, recursive=args.recursive)
+        delete_vms(vmx_paths=args.vm_directory_path, target_running=args.target_running, recursive=args.recursive)
     
     if args.stop_vms:
-        stop_vms(vmx_paths=args.vm_directory_path, target_running=args.target_running)
+        stop_vms(vmx_paths=args.vm_directory_path, target_running=args.target_running, recursive=args.recursive)
 
     if args.suspend_vms:
-        suspend_vms(vmx_paths=args.vm_directory_path, target_running=args.target_running)
+        suspend_vms(vmx_paths=args.vm_directory_path, target_running=args.target_running, recursive=args.recursive)
 
     if args.list_running_vms:
         running_vms = sorted(list_running_vms())
@@ -202,17 +249,20 @@ if __name__ == "__main__":
         for i in running_vms:
             print(i)
 
+    if args.convert_to_ova:
+        convert_vmx_to_ova(path=args.vm_directory_path, target_running=args.target_running, recursive=args.recursive)
+
     if args.list_snapshots:
-        list_snapshots(vmx_paths=args.vm_directory_path, target_running=args.target_running)
+        list_snapshots(vmx_paths=args.vm_directory_path, target_running=args.target_running, recursive=args.recursive)
 
     if args.create_snapshot:
-        create_snapshot(vmx_paths=args.vm_directory_path, target_running=args.target_running, snapshot_name=args.create_snapshot)
+        create_snapshot(vmx_paths=args.vm_directory_path, target_running=args.target_running, snapshot_name=args.create_snapshot, recursive=args.recursive)
     
     if args.restore_snapshot:
-        restore_snapshot(vmx_paths=args.vm_directory_path, target_running=args.target_running, snapshot_name=args.restore_snapshot)
+        restore_snapshot(vmx_paths=args.vm_directory_path, target_running=args.target_running, snapshot_name=args.restore_snapshot, recursive=args.recursive)
 
     if args.delete_snapshot:
-        delete_snapshot(vmx_paths=args.vm_directory_path, target_running=args.target_running, snapshot_name=args.delete_snapshot)
+        delete_snapshot(vmx_paths=args.vm_directory_path, target_running=args.target_running, snapshot_name=args.delete_snapshot, recursive=args.recursive)
 
     #if args.download_homelab:
     #    path = pathlib.Path(args.vm_directory_path)
